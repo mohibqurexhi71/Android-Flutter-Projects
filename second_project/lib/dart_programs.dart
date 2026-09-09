7bf50a9bd7b3c531ac6af7781a473c7d0185d5a3
Mohib uddin <qurexhimohib71>
2026-09-09 22:13:18 +0500

    second projects basics of UI

---
+++ second_project/lib/dart_programs.dart
@@ -0,0 +1,92 @@
+// void main(){
+// print('*');
+// print('**');
+// print('***');
+// print('****');
+
+// const num1 = 5;
+// const num2 = 10;
+// print(num1 + num2);
+// print("Hello World");
+//
+// const num3 = num1;
+// print(num3);
+
+// double dec2 = 3.456;
+// double dec1 = 4.645;
+// double divid = (dec1 * dec2);
+// print(divid);
+
+// int x = 10;
+// int y = 20;
+// print("Before Swapping");
+// print("x -> $x");
+// print("y -> $y");
+// // int temp = x;
+// // x = y;
+// // y = temp;
+// x = x + y;
+// y = x - y;
+// x = x - y;
+//
+// print("After Swaping");
+// print("x -> $x");
+// print("y -> $y");
+
+// Converting Feet into Meters and Meters into KM
+// double feets = 6;
+// double meters , km;
+// meters = feets / 3;
+// km = meters / 1000;
+// print('feets -> $feets');
+// print('meters -> $meters');
+// print('km -> $km');
+
+// String  str = 'Hello world';
+// String intro = 'Hello i am Captain America';
+// print(str.length);
+// print(str[6]);
+// String result;
+// result = str + intro ;
+// print(result);
+// print(intro.toLowerCase());
+// print(intro.toUpperCase());
+// print(intro.toUpperCase().[7]);
+// print(str.toUpperCase()[6]); //To target just one index
+// // print(str.toUpperCase().substring(6,11)); //For target srat to nth index
+// String Intro = intro.replaceAll('Captain America' ,' Solider Boy');
+// print(Intro);
+// print(Intro.split(''));
+// print(Intro.split(""));
+
+
+// Boolean
+// int number_1 = 10;
+// int number_2 = 20;
+// bool result = true;
+// bool isEven = true;
+// // print(number_1 < number_2);
+// // isEven = number_1 / 2 == 0;
+//  isEven = number_1 / number_2 == 0 ;
+// print(isEven);
+
+// Dynamic DataType
+//
+// const data_1 = 45;
+// data_1 = 43; //its not redeclear bcz we give constant dataType
+
+// var data_2 =  45;
+// data_2 = 344;//we can declear bcz this is a global DataType
+
+// var data;
+// data = [1,2,3];//list
+// print(data);
+
+// var data_3 ;
+// data_3 = {'map':1 , 'data':2};
+// print(data_3('map'));
+
+
+// }
+
+
